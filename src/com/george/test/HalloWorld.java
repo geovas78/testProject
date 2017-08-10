ab7151abe25066b13bf6ff152dd946ccc543bb6b
Georgi Vasilski <jvjordanvip@gmail.com>
2017-08-10 20:41:46 +0100

    First implementation

---
+++ src/com/george/test/HalloWorld.java
@@ -0,0 +1,10 @@
+package com.george.test;
+
+public class HalloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hallo World!");
+	}
+
+}
