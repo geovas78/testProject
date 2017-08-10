44de7a763ac0a17c216d0ff44b11bf0b0054f7e1
Georgi Vasilski <jvjordanvip@gmail.com>
2017-08-10 20:47:28 +0100

    branched implementation

--- src/com/george/test/HalloWorld.java
+++ src/com/george/test/HalloWorld.java
@@ -4,7 +4,7 @@ public class HalloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hallo World!");
+		System.out.println("This is a new implementation");
 	}
 
 }
